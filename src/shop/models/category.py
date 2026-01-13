41d60b403526c333950521b792d50e435c68f71f
MicaelaGrande <micaela.grande@mi.unc.edu.ar>
2026-01-13 00:37:33 -0300

    Nueva relacion entre products y category

--- src/shop/models/category.py
+++ src/shop/models/category.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Boolean, Integer, String
 from src.shop.db import Base
-
+from sqlalchemy.orm import relationship
 
 class Category(Base):
     __tablename__ = "categories"
@@ -8,3 +8,5 @@ class Category(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), nullable=False, unique=True)
     is_active = Column(Boolean, default=True)
+
+    products= relationship("Product", secondary="product_category", back_populates="categories" )
--- src/shop/models/products.py
+++ src/shop/models/products.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Numeric, Text, Boolean
 from src.shop.db import Base
+from sqlalchemy.orm import relationship
 
 
 class Product(Base):
@@ -10,3 +11,5 @@ class Product(Base):
     description = Column(Text, nullable=False)
     price = Column(Numeric(10, 2), nullable=False)
     is_active = Column(Boolean, default=True)
+
+    categories = relationship("Category", secondary="product_category", back_populates="products")
