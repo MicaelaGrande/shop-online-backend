ff4d1c990e0e9f9122583d0d9ca55ff385858fcf
MicaelaGrande <micaela.grande@mi.unc.edu.ar>
2026-01-19 22:55:05 -0300

    Nuevo schema +  validaciones y ajustes en los endpointas

--- src/shop/endpoints/products.py
+++ src/shop/endpoints/products.py
@@ -1,21 +1,61 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+
 from src.shop.db import get_db
-from src.shop.models.products import Product
-from src.shop.schemas.products import ProductPublic
+from src.shop.models import Product, Category, Media
+from src.shop.schemas.products import ProductPublic, ProductCreate
+from fastapi import HTTPException
 
-router = APIRouter()
+router = APIRouter(prefix="/products", tags=["products"])
 
-#Nota mental de funcionamiento: Cuando llegue un GET a /products, llamá a get_products función
-@router.get(
-    "/products",
-    response_model=list[ProductPublic]
-)
+
+# Nota mental de funcionamiento: Cuando llegue un GET a /products, llamá a get_products función
+@router.get("/", response_model=list[ProductPublic])
 def get_products(db: Session = Depends(get_db)):
-    products = (
-        db.query(Product)
-        .filter(Product.is_active == True)
-        .all()
-    )
+    products = db.query(Product).filter(Product.is_active == True).all()
     return products
+
+
+@router.post("/", response_model=ProductPublic)
+def create_product(product_in: ProductCreate, db: Session = Depends(get_db)):
+    existing = (
+        db.query(Product)
+        .filter(Product.name == product_in.name, Product.is_active == True)
+        .first()
+    )
+
+    if existing:
+        raise HTTPException(
+            status_code=400, detail="Ya existe un producto activo con ese nombre"
+        )
+
+    product = Product(
+        name=product_in.name, description=product_in.description, price=product_in.price
+    )
+
+    if product_in.category_ids:
+        categories = (
+            db.query(Category).filter(Category.id.in_(product_in.category_ids)).all()
+        )
+        product.categories = categories
+
+        if len(categories) != len(product_in.category_ids):
+            raise HTTPException(
+                status_code=400, detail="Una o más categorías no existen"
+            )
+
+        inactive = [c.name for c in categories if not c.is_active]
+        if inactive:
+            raise HTTPException(
+                status_code=400, detail=f"Categorias inactivas: {', '.join(inactive)}"
+            )
+
+    if product_in.media_urls:
+        product.media = [Media(url=url) for url in product_in.media_urls]
+
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+
+    return product
--- src/shop/schemas/products.py
+++ src/shop/schemas/products.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from decimal import Decimal
-from typing import List
+from typing import List, Optional
 from .category import CategoryPublic
 from .media import MediaPublic
 
@@ -15,3 +15,35 @@ class ProductPublic(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class ProductCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: Decimal = Field(gt=0)  # Debe ser price>0
+    category_ids: Optional[List[int]] = []
+    media_urls: Optional[List[str]] = []
+
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, name):
+        if not name or not name.strip():
+            raise ValueError("El nombre del producto no puede estar vacío")
+
+        if len(name.strip()) < 3:
+            raise ValueError("El nombre del producto debe tener al menos 3 caracteres")
+
+        if len(name) > 100:
+            raise ValueError(
+                "El nombre del producto no puede superar los 100 caracteres"
+            )
+
+        return name.strip().lower()
+
+    @field_validator("media_urls")
+    @classmethod
+    def validate_media_urls(cls, urls):
+        for url in urls:
+            if not url or not url.strip():
+                raise ValueError("URL/s invalida/s")
+        return urls
