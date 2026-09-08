56e049a6639c5cb0970b053924a330ca013e15c7
Micaela Grande <micaela.grande@mi.unc.edu.ar>
2026-09-08 18:26:54 -0300

    chore(backend): configure Cloudinary dependencies and environment

--- src/settings.py
+++ src/settings.py
@@ -10,5 +10,13 @@ class Settings(BaseSettings):
     # Configuración del servidor
     DEBUG_MODE: bool = True
 
+    # Cloudinary
+    CLOUDINARY_CLOUD_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
+
+    class Config:
+        env_file = ".env"
+
 
 settings = Settings()
